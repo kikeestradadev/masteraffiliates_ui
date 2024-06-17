24d9dc3c728bda15cb24ed166de06b7f7d5a0e02
kikeestrada <kikeestradadev@gmail.com>
2024-06-17 13:27:58 -0600

    Updates

--- index-dist.js
+++ index-dist.js
@@ -6,23 +6,15 @@
 /***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
 
 __webpack_require__.r(__webpack_exports__);
-var coreModule = function coreModule() {
-  console.log('Hola Core Module');
-};
-/* harmony default export */ __webpack_exports__["default"] = (coreModule);
-
-/***/ }),
-/* 2 */
-/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
-
-__webpack_require__.r(__webpack_exports__);
 var topNav = function topNav() {
   document.addEventListener('DOMContentLoaded', function () {
     var hamburger = document.querySelector('.top-nav__mobile-hamburguer');
     var menuContainer = document.querySelector('.top-nav__menu-container');
+    var body = document.body;
     if (hamburger && menuContainer) {
       hamburger.addEventListener('click', function () {
         menuContainer.classList.toggle('active');
+        body.classList.toggle('active');
         console.log('Hamburger menu toggled');
       });
     } else {
@@ -31,16 +23,6 @@ var topNav = function topNav() {
   });
 };
 /* harmony default export */ __webpack_exports__["default"] = (topNav);
-
-/***/ }),
-/* 3 */
-/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
-
-__webpack_require__.r(__webpack_exports__);
-var z = function z() {
-  console.log('Hola internal Module');
-};
-/* harmony default export */ __webpack_exports__["default"] = (internalModule);
 
 /***/ })
 /******/ 	]);
@@ -86,26 +68,25 @@ var __webpack_exports__ = {};
 // This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
 !function() {
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _core_modules_coreModule__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(1);
-/* harmony import */ var _core_modules_topNav__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(2);
-/* harmony import */ var _internal_modules_internalModule__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(3);
+/* harmony import */ var _core_modules_topNav__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(1);
 /*here start core layout ui scripts imports*/
-
+// import coreModule from './core-modules/coreModule';
 
 /*here finish core layout ui scripts imports*/
 
 /*here start internal layout ui components scripts imports*/
-
+// import internalModule from './internal-modules/internalModule';
 /*here finish internal layout ui components scripts imports*/
 
 (function () {
   /*here start core layout ui scripts functions*/
-  (0,_core_modules_coreModule__WEBPACK_IMPORTED_MODULE_0__["default"])();
-  (0,_core_modules_topNav__WEBPACK_IMPORTED_MODULE_1__.topNav)();
+  // coreModule();
+  (0,_core_modules_topNav__WEBPACK_IMPORTED_MODULE_0__["default"])();
   /*here finish core layout ui scripts functions*/
 })();
 (function () {
-  (0,_internal_modules_internalModule__WEBPACK_IMPORTED_MODULE_2__["default"])();
+  // internalModule();
+  console.log('Hola kike');
 })();
 }();
 /******/ })()
